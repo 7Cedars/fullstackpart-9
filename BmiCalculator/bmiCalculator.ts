5db2c20201a508cf0ff39762d4f9356f19fe8ae5
7Cedars <Cedars7@proton.me>
2023-07-26 15:16:42 +0100

    Exercise 9.5

--- BmiCalculator/bmiCalculator.ts
+++ BmiCalculator/bmiCalculator.ts
@@ -1,42 +1,63 @@
-import {isNotNumber, Readline} from './utils' 
+// import {isNotNumber} from './utils' 
 
-const calculateBmi = (height: number, weight: number): string => {
-
-  // calculation and assessment categories taken from https://en.wikipedia.org/wiki/Body_mass_index
-  const BMI = weight / ((height / 100)^2)
-  console.log("BMI: ", BMI)
-  
-  if (BMI < 16) { return "Underweight (Severe thinness)" }
-  if (BMI >= 16 && BMI < 16.9) { return "Underweight (Moderate thinness)" }
-  if (BMI >= 17 && BMI < 18.4) { return "Underweight (Mild thinness)" }
-  if (BMI >= 18.5 && BMI < 24.9) { return "Normal range" }
-  if (BMI >= 25 && BMI < 29.9) { return "Overweight (Pre-obese)" }
-  if (BMI >= 30 && BMI < 34.9) { return "Obese (Class I)" }
-  if (BMI >= 35 && BMI < 39.9) { return "Obese (Class II)" }
-  if (BMI >= 40) { return "Obese (Class III)" }
-  else {
-    return "Outside of assessed BMI range."
-  }
+interface BmiAssessment {
+  weight: number,
+  height: number,
+  bmiRating: number, 
+  bmi: string
 }
 
-Readline.question("Enter your height (in cm) and weight (in kg), divided by a space.\n", (answer: string) => {
-  const userInput = answer.split(" ")
-  if (userInput.length != 2 ) {
-    console.log("incorrect number of inputs. Insert two numbers divided by a space. Do not add a space at end of sentence." ) 
-    Readline.close();
-  } 
-  if (isNotNumber(userInput[0]) === true || isNotNumber(userInput[1]) === true) {
-    console.log("incorrect type of input. Insert two numbers. Do not add a space at end of sentence." ) 
-    Readline.close();
-  }   
-  if (isNotNumber(userInput[0]) === false && isNotNumber(userInput[1]) === false) {
-    const height = Number(userInput[0])
-    const weight = Number(userInput[1])
-    console.log(calculateBmi(height, weight))
-    Readline.close();
-  } 
+export const calculateBmi = (height: number, weight: number): BmiAssessment | string => {
+
+  // calculation and assessment categories taken from https://en.wikipedia.org/wiki/Body_mass_index (and later simplified)
+  try {
+    const bmiRating = weight / ((height / 100)^2)
+
+    let bmi = " " 
+    if (bmiRating < 18.5) { bmi = "Underweight" }
+    if (bmiRating >= 18.5 && bmiRating < 25) { bmi =  "Normal range" }
+    if (bmiRating > 25) { bmi = "Overweight" }
+
+    return ({
+      weight,
+      height,
+      bmiRating,
+      bmi
+    })
+
+  } catch (error) {
+    return(`${error}: malformatted parameters`)
   }
-)
+
+// export const calculateBmi = (input: string): BmiAssessment | string => {
+  
+//   const userInput = input.split(" ")
+
+//   try {
+//     const height = parseInt(userInput[0])
+//     const weight = parseInt(userInput[1])
+//     return(bmiCalculator(height, weight))
+//   } catch (error) {
+//     return(`${error}: malformatted parameters`)
+//   }
+  // if (userInput.length != 2 ) {
+  //   return("incorrect number of inputs. Insert two numbers divided by a space. Do not add a space at end of sentence." ) 
+  // }
+
+  // if (isNotNumber(userInput[0]) === true || isNotNumber(userInput[1]) === true) {
+  //   return("incorrect type of input. Insert two numbers. Do not add a space at end of sentence." ) 
+  // }   
+
+  // if (isNotNumber(userInput[0]) === false && isNotNumber(userInput[1]) === false) {
+  //   const height = Number(userInput[0])
+  //   const weight = Number(userInput[1])
+  //   return(bmiCalculator(height, weight))
+  // }
+
+  // return 
+}
+
+export default "default"
 
 
 
--- BmiCalculator/index.ts
+++ BmiCalculator/index.ts
@@ -1,11 +1,22 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+
 const app = express();
 
-app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
+app.get('/bmi', (request, response) => {
+
+  const height = Number(request.query.height)
+  const weight = Number(request.query.weight) 
+
+  if (Number.isNaN(height) || height === 0 || Number.isNaN(weight) || weight === 0 ) {
+    response.status(500).send(`Error: malformatted parameters!`)
+  } else {
+    const res = calculateBmi(height, weight)
+    response.send(res);
+  } 
 });
 
-const PORT = 3003;
+const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
