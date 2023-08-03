df1dd4b6a21f62dc1ae662ebd523db609ccec4a3
7Cedars <Cedars7@proton.me>
2023-08-03 12:19:12 +0100

    Exercise 9.18 (clean up backend)

--- flight-diary-frontend/src/services/diaryServices.ts
+++ flight-diary-frontend/src/services/diaryServices.ts
@@ -10,18 +10,6 @@ export const getAllEntries = async () => {
 };
 
 export const createEntry = async (entryToAdd: NewDiaryEntry) => {
-  try {
-    const response = axios.post<NewDiaryEntry>(baseUrl, entryToAdd ) 
-    return response;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.log("THIS IS AN ERROR!", error.status)
-      return "this is an error"
-      // console.error(error.response);
-      // Do something with this error...
-    } else {
-      console.log("THIS IS AN ERROR!", error)
-      // console.error(error);
-    }
-  }
+  const response = axios.post<NewDiaryEntry>(baseUrl, entryToAdd ) 
+  return response;
 }; 
